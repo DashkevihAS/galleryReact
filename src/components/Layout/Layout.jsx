f3cc4233df6b0e50a8ad16e915715ceec2ad35be
DashkevichAS <antonsnov0604@gmail.com>
2022-08-22 12:49:45 +0300

    begin

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from './Layout.module.css';
+
+export const Layout = () => {
+  console.log(style);
+  return (
+    <div></div>
+  );
+};
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from './Main.module.css';
+
+export const Main = () => {
+  console.log(style);
+  return (
+    <div></div>
+  );
+};
