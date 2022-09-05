e55f09bb04e493d0711e09b903f6d3438768b6b0
DashkevichAS <antonsnov0604@gmail.com>
2022-09-05 13:34:40 +0300

    fix const REDIRECT

--- src/api/const.js
+++ src/api/const.js
@@ -1,6 +1,6 @@
 export const API_URL_AUTH = 'https://www.unsplash.com/oauth/authorize?';
 export const ACCESS_KEY = 'RsAgvX7aIapnoALkfM5cAHXoNWhn1ujD6V-soXcFTQE';
-export const REDIRECT_URI = 'http://192.168.0.150:3000/';
+export const REDIRECT_URI = 'https://gallery-react-blond.vercel.app/';
 export const RESPONSE_TYPE = 'code';
 export const SCOPE = 'public read_user read_photos write_likes';
 
